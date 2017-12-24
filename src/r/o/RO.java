0901e055e58f0f7c30f7c3fdc01dbe04c04e1fee
rachinam <aryan.kona@live.fr>
2017-12-24 15:33:12 +0100

    calcule new table

--- src/r/o/RO.java
+++ src/r/o/RO.java
@@ -5,12 +5,20 @@ public class RO {
     public static void main(String[] args) {
         
         Simplex<Integer> a = new Simplex<Integer>();
-        a.addZ(15, 10);
-        a.addCont(1, 2);
-        a.addCont(3, 4);
-        a.addCont(5, 6);
-        a.affiche();
+        a.setContrains(5,4,0);
+        a.setContrains(2,3,10);
+        a.setContrains(4,10,12);
+
+        //------------------------
         
+       int tab1 [] =  new  int [] {0,  1,  2,  3,  4,  5} ;  
+        int tab2 [] = null ; 
+
+       System.arraycopy(tab1,  1, tab2,  1,  0) ;
+       
+        System.out.println("u1 :"+tab2[0]+" u4 "+tab2[1] );
+         System.out.println("u3 :"+tab2[2]+" u5 "+tab2[3] );
+          System.out.println("u3 :"+tab2[4]+" u5 "+tab2[5] );
     }
     
 }
--- src/r/o/Simplex.java
+++ src/r/o/Simplex.java
@@ -7,15 +7,75 @@ public class Simplex<T> {
     private double [][]table;
     
     public Simplex(){
-        this.colm = 2;
-        this.rows = 2;
-        this.table = new double[2][2];
+        this.colm = 3;
+        this.rows = 3;
+        this.table = new double[3][3];
     }
     
     public Simplex(int colm ,int rows){
-        this.colm = colm;
-        this.rows = rows;
-        this.table = new double[colm][rows];
+        this.colm = colm+1;
+        this.rows = rows+1;
+        this.table = new double[this.colm][this.rows];
+    }
+    
+    public void setContrains(int a1 ,int a2,int z){
+        for (int i = 0; i < this.table.length; i++) {
+            if(this.table[i][0] == 0){
+                {
+                    this.table[i][0] = a1;
+                    this.table[i][1] = a2;
+                    this.table[i][2] = z;   
+                }
+                break;
+            }
+        }
+    }
+    
+    public double getMax(){ // get max Z 
+        double max = this.table[0][0];
+        int ktr;
+        for (ktr = 0; ktr < this.table[0].length; ktr++) {
+            if (this.table[0][ktr]> max) {
+                max = this.table[0][ktr];
+            }
+        }
+        return max;
+    }
+    
+    public int findPosition(double max){
+        for (int i = 0; i < this.table[0].length; i++) {
+            if(this.table[0][i] == max){
+                return i;
+            }
+        }
+        return 0;
+    }
+    
+    public double getVariable(){
+        double v1 = this.table[1][2];
+        double v2  =this.table[2][2];
+  
+        v1 = v1/this.table[1][findPosition(getMax())];
+        v2 = v2/this.table[2][findPosition(getMax())];
+        if(v1 > v2){
+            return v2;
+        }else{
+            return v1;
+        }    
+    }
+    
+    public void calculeNewTable(){
+        
+    }
+    
+    
+    public void aff(){
+        for (int i = 0; i < this.table.length; i++) {
+            for (int j = 0; j < this.table[i].length; j++) {
+                System.out.print(this.table[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
     
     
